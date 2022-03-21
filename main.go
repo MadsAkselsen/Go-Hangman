a70a6d26d45e35dd73ab1209bef297c3b85a1398
Mads Akselsen <akselsenmads@gmail.com>
2022-03-21 07:17:12 +0100

    description

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// Printing game state
+	//	* Print word you're guessing
+	//	* Print hangman state
+	// Derive a word we have to guess
+	// Read user input
+	// * validate it (e.g. only letters)
+	// Determine if the letter is a correct guess or not
+	//	* if correct, update guessed letters
+	//	* if incorrect, update the hangman state
+	// If word is guessed -> game over, you win
+	// If hangman is complete -> game over, you lose
+}
